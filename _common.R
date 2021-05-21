22fac25f4e18891e316a9d0f9e9be8aad09bcf9c
zhongyf <598253220@qq.com>
2021-05-21 18:39:28 +0800

    commit

--- _common.R
+++ _common.R
@@ -4,8 +4,7 @@ options(digits = 3)
 knitr::opts_chunk$set(
   comment = "#>",
   collapse = TRUE,
- # cache = TRUE,
-  out.width = "70%",
+  out.width = "90%",
   fig.align = 'center',
   fig.width = 6,
   fig.asp = 0.618,  # 1 / phi
